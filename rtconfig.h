8b3ddb2c1853f78397fcfe0268b1d3be0d712b91
SevenQi <2586172363@qq.com>
2023-03-13 09:11:26 +0800

    .

--- rtconfig.h
+++ rtconfig.h
@@ -130,12 +130,6 @@
 
 /* LVGL: powerful and easy-to-use embedded GUI library */
 
-#define PKG_USING_LVGL
-#define PKG_LVGL_THREAD_PRIO 20
-#define PKG_LVGL_THREAD_STACK_SIZE 4096
-#define PKG_LVGL_DISP_REFR_PERIOD 5
-#define PKG_LVGL_USING_V08034
-#define PKG_LVGL_VER_NUM 0x08034
 
 /* u8g2: a monochrome graphic library */
 
