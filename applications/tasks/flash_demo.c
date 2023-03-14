e55187697b2369a1dc332ccae8567e6dd5ae452f
SevenQi <2586172363@qq.com>
2023-03-14 16:16:34 +0800

    .

--- applications/tasks/flash_demo.c
+++ applications/tasks/flash_demo.c
@@ -8,9 +8,11 @@ int read_data(void){
     fal_init();
     struct fal_partition *falPartition= fal_partition_find("easyflash");
     uint8_t buf[8];
+//    rt_uint32_t erase_size = fal_partition_erase_all(falPartition);
     rt_uint32_t size = fal_partition_read(falPartition,0,buf,8);
+    fal_partition_write(falPartition,0,buf,8);
     for (int i = 0; i < size; ++i) {
-        rt_kprintf("%d",buf[i]);
+        rt_kprintf("%2x\t\t",buf[i]);
     }
     return RT_EOK;
 }
--- board/w25x16/drv_w25x16_flash.c
+++ board/w25x16/drv_w25x16_flash.c
@@ -8,10 +8,8 @@
 
 static int rt_hw_spi_flash_init(void){
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    int result = rt_hw_spi_device_attach("spi1","spi10",GPIOA,GPIO_PIN_2);
-    rt_kprintf("result:%d",result);
+    rt_hw_spi_device_attach("spi1","spi10",GPIOA,GPIO_PIN_2);
     if(rt_sfud_flash_probe("w25x16","spi10") == RT_NULL){
-        rt_kprintf("failed w25x16 init!!!!!!1");
         return -RT_ERROR;
     }
     return RT_EOK;
