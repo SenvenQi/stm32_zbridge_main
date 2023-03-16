f5f9c60a531768ed60c949371dda412e93929ec2
SenvenQi <542559103@qq.com>
2023-03-17 01:49:53 +0800

    .

--- applications/tasks/uart1_task.c
+++ applications/tasks/uart1_task.c
@@ -31,18 +31,7 @@ void lan_callback(void *parameter){
                 data[i] = rx_buffer[i];
             }
             uart_filter(data,size);
-//            for (rt_uint16_t i = 0; i < size; i++)
-//            {
-//                rt_kprintf("%2x",rx_buffer[i]);
-//                rx_buffer[i] = '\0';
-//            }
-
             rt_kprintf("%d-%s\t",size,data);
-//             write_font(offset,rx_buffer,size);
-//             offset += size;
-//            if (size >= 8){
-//                lan_handler();
-//            }
         }
     }
 }
