10d7063d5c52e6061fb28424e580f8dcab732120
SenvenQi <542559103@qq.com>
2023-03-17 02:00:28 +0800

    .

--- applications/config/lcd_pwm_config.c
+++ applications/config/lcd_pwm_config.c
@@ -5,6 +5,7 @@
 rt_uint32_t period = 500000, /* 周期为0.5ms，单位为纳秒ns */
 pulse = 500000,  /* PWM脉冲宽度值的增减方向 */
 dir = 0;  /* PWM脉冲宽度值，单位为纳秒ns */
+struct rt_device_pwm *pwm_dev_lcd;
 int lcd_pwm_config(void){
     pwm_dev_lcd = (struct rt_device_pwm *)rt_device_find(LCD_PWM_DEV_NAME);
     /* 设置PWM周期和脉冲宽度默认值 */
