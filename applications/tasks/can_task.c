be3f0d42ef1776630756f9d2ccc8cf19e3a1d295
SenvenQi <542559103@qq.com>
2023-03-17 01:50:36 +0800

    .

--- applications/tasks/can_task.c
+++ applications/tasks/can_task.c
@@ -1,13 +1,12 @@
 //
 // Created by 25861 on 2023/3/5.
 //
-#include "protocols/can_msg.h"
 #include "config/device.h"
 
 void can_handler(){
-    rt_uint8_t msg1[8]= {0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88};
-    struct rt_can_msg msg = create_can_msg(0x78,msg1);
-    rt_device_write(can_dev,0,&msg,sizeof msg);
+//    rt_uint8_t msg1[8]= {0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88};
+//    struct rt_can_msg msg = create_can_msg(0x78,msg1);
+//    rt_device_write(can_dev,0,&msg,sizeof msg);
 }
 
 void can_callback(void *parameter){
